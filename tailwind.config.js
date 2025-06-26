e002f267ff74be6b5d868419013a8eaabe816b26
darunporn-san <83495267+darunporn-san@users.noreply.github.com>
2025-06-26 05:22:42 +0000

    cdn tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,vue,html}",
   ],
   theme: {
     extend: {},
